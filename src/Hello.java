60c8aa60328413ffd83d1a0cc9d0a3ad47c24373
Matt Thomas <matt@betweenbrain.com>
2016-01-15 14:03:55 -0500

    Basic use variables

--- src/Hello.java
+++ src/Hello.java
@@ -7,7 +7,17 @@
  */
 
 public class Hello {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber =  myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int diff = 1000 - myTotal;
+
+        System.out.println(diff);
     }
 }
