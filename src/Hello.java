7884d9bf3f3eacce9210da466edf3fce36c189c8
Matt Thomas <matt@betweenbrain.com>
2016-01-15 13:44:13 -0500

    Update file block

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,11 @@
 /**
- * Created by mthomas on 1/12/16.
+ * Project    HelloWorld
+ * Created    1/12/16 1:43 PM
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Copyright  Copyright (C) 2016 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v2 or later
  */
+
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello, World!");
